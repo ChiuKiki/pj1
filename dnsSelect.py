ee81a790a76fa80f86dc1e096098ef65db34f950
kiki <840084384@qq.com>
2022-04-12 13:05:16 +0800

    用redis实现动态选择、在主程序中新增攻击代码

--- dnsSelect.py
+++ dnsSelect.py
@@ -1,10 +1,32 @@
 # 随机选出3个DNS
-
-import random
+import copy
+import redis
 
 
 def dns_select():
-    dns_ip = ['8.8.8.8', '8.8.8.8', '8.8.8.8', '8.8.8.8']
+    dns_ip = ['8.8.8.8', '8.8.4.4', '1.2.4.8', '210.2.4.8']
     # dns_ip = ['116.57.77.213', '116.57.77.214', '116.57.77.215', '116.57.77.216']
-    ip_selected = random.sample(dns_ip, 3)
-    return ip_selected
+    # redis数据库中找出被攻击次数最多的DNS服务器
+    pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    server = r.zrevrange("server_zset", 0, 0)
+    print('所有DNS服务器ip：' + str(dns_ip))
+    print('有序集合(\'值\', 分数)：' + str(r.zrevrange("server_zset", 0, -1, withscores=True)))
+    print('出错最多的服务器：'+str(server[0]))
+    rm = -1
+    if server[0] == 'server_0':
+        rm = 0
+    elif server[0] == 'server_1':
+        rm = 1
+    elif server[0] == 'server_2':
+        rm = 2
+    elif server[0] == 'server_3':
+        rm = 3
+    dns_ip_cp = copy.deepcopy(dns_ip)
+    del(dns_ip_cp[rm])
+    print('动态选择结果：'+str(dns_ip_cp))
+    return rm, dns_ip_cp, dns_ip
+
+
+if __name__ == '__main__':
+    dns_select()
--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from ruling import ruling
 import copy
 import time
 import random
+import redis
 
 
 def main(domain, dnsType):
-    ip = dns_select()
+    selected = dns_select()
+    rm = selected[0]        # 不选择的服务器下标
+    ip = selected[1]        # 选择的三台服务器ip
+    dns_ip = selected[2]    # 四台服务器
     rd = random.randrange(10)   # 随机生成出错的DNS服务器的ip下标，出错概率为3/10
+    if rd < 4 and rd != rm:
+        db(rd)  # 数据库记录出错的DNS服务器ip
     if dnsType == 'A' or dnsType == 'NS' or dnsType == 'CNAME':
         res1 = []
         for i in range(3):
             try:
-                if i == rd:     # 随机错误
+                if rd < 4 and ip[i] == dns_ip[rd]:     # 随机错误
                     res1.append(['err'])
                 else:
                     res1.append(rsv(ip[i], domain, dnsType))  # 三个DNS服务器解析的A记录结果
@@ -31,7 +37,7 @@ def main(domain, dnsType):
         res2 = []
         for i in range(3):
             try:
-                if i == rd:     # 随机错误
+                if rd < 4 and ip[i] == dns_ip[rd]:     # 随机错误
                     res2.append([['err1'], ['err2']])
                 else:
                     res2.append(rsv(ip[i], domain, dnsType))
@@ -69,10 +75,28 @@ def main(domain, dnsType):
         print('输入错误')
 
 
+# 记录上次出错的DNS服务器
+def db(i):
+    pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    r.zincrby("server_zset", 1, "server_"+str(i))
+    print(r.zrevrange("server_zset", 0, -1, withscores=True))
+
+
+# 初始化数据库
+def init():
+    pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    r.flushall()
+    r.zadd("server_zset", mapping={"server_0": 0, "server_1": 0, "server_2": 0, "server_3": 0})
+
+
 if __name__ == '__main__':
+    # init()
     domain = input('输入查询域名(DNS记录类型若为CNAME则需要加上www.):')
     dnsType = input('请输入DNS记录类型(A,MX,NS,CNAME)：')
     start = time.time()  # 开始计时
     main(domain, dnsType)
     end = time.time()  # 结束计时
     print('运行时间：%s 秒' % (end - start))
+
